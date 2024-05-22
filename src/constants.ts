77b09d9fa55e8dcc0e7e8bfc2fa500d13abfb55a
tomgif <tomgif@rambler.ru>
2024-05-22 13:58:46 +0300

    build

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const TITLE: {
-  [key: string]: string
-} = {
-  programmers: 'Список разработчиков',
-  managers: 'Список менеджеров',
-  testers: 'Списко тестеровщиков'
-};
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import './style.css';
-import App from './App.vue';
-
-createApp(App)
-  .use(ElementPlus)
-  .mount('#app');
